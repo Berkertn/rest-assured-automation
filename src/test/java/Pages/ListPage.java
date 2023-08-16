bc9b615d32a3ddcf3d916674e9c446cb35c86a00
Berk Ertan <berk.ertan@commencis.com>
2023-08-17 01:49:42 +0300

    ListPage added

---
+++ src/test/java/Pages/ListPage.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class ListPage extends BasePage {
+    public ListPage(String name, String boardId) {
+        super(name);
+        createURL = String.format("/1/boards/%s/lists", boardId);
+    }
+}
