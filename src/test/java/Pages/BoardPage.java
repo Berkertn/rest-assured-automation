c30ec1453851bed2199d5f09b3017c06da7f171f
Berk Ertan <berk.ertan@commencis.com>
2023-08-17 02:18:45 +0300

    BoardPage.java got boardID updates

--- src/test/java/Pages/BoardPage.java
+++ src/test/java/Pages/BoardPage.java
@@ -1,10 +1,23 @@
 package Pages;
 
-public class BoardPage extends BasePage{
+public class BoardPage extends BasePage {
+    private String boardID;
 
     public BoardPage(String name) {
         super(name);
         createURL = "/1/boards";
-        deleteURL = "/1/boards/"; // + boardID
+    }
+
+    public BoardPage() {
+        createURL = "/1/cards";
+    }
+
+    public void setBoardID(String boardID) {
+        this.boardID = boardID;
+        deleteURL = "/1/boards/" + boardID;
+    }
+
+    public String getBoardID() {
+        return boardID;
     }
 }
