4b56cf46ca77af776b6162b437f98cffe037cca8
Berk Ertan <berk.ertan@commencis.com>
2023-08-17 01:43:45 +0300

    POM updated

--- src/test/java/Pages/BoardPage.java
+++ src/test/java/Pages/BoardPage.java
@@ -2,4 +2,9 @@ package Pages;
 
 public class BoardPage extends BasePage{
 
+    public BoardPage(String name) {
+        super(name);
+        createURL = "/1/boards";
+        deleteURL = "/1/boards/"; // + boardID
+    }
 }
