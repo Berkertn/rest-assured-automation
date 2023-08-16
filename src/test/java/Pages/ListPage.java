34d44619b5c8289f4d4d0fca8366638c8604a9a3
Berk Ertan <berk.ertan@commencis.com>
2023-08-17 02:22:28 +0300

    ListPage.java got listID updates

--- src/test/java/Pages/ListPage.java
+++ src/test/java/Pages/ListPage.java
@@ -1,9 +1,22 @@
 package Pages;
 
 public class ListPage extends BasePage {
+    private String listID;
+
     public ListPage(String name, String boardId) {
         super(name);
         createURL = String.format("/1/boards/%s/lists", boardId);
+    }
 
+    public ListPage() {
+        createURL = "/1/cards";
+    }
+
+    public void setListID(String listID) {
+        this.listID = listID;
+    }
+
+    public String getListID() {
+        return listID;
     }
 }
